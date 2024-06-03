9474326888250269473dcbe2064c72166453357c
Aminemh <mahdiamine@outlook.com>
2024-06-03 15:12:39 +0200

    modifying icons in the list

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,6 @@
     <script setup>
-    const props = defineProps({
-        todo :{
-            type : Object ,
-            required : true,
-        },
-    });
+        import { defineComponent } from 'vue';
+
     </script>
 <template>
     <li>
@@ -15,7 +11,11 @@
                 {{ todo.todo }}
             </span>
         </div>
-        <div class="todo-actions"></div>
+        <div class="todo-actions">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px" viewBox="0 0 256 256">
+		<path fill="#41b080" d="M173.66 98.34a8 8 0 0 1 0 11.32l-56 56a8 8 0 0 1-11.32 0l-24-24a8 8 0 0 1 11.32-11.32L112 148.69l50.34-50.35a8 8 0 0 1 11.32 0M232 128A104 104 0 1 1 128 24a104.11 104.11 0 0 1 104 104m-16 0a88 88 0 1 0-88 88a88.1 88.1 0 0 0 88-88" />
+	</svg>
+        </div>
     </li>
 </template>
 
